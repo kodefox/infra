5cad23c9e845d3ef08d2f238e247b71e8bdd7934
Yosua Ian Sebastian <yosua.ian.sebastian@gmail.com>
2019-08-14 21:15:30 +0700

    Init Exoflex (#5)
    * init exoflex
    
    * add react and react-native as peer deps
    
    * add react-native-paper as deps
    
    * change peer deps to use react with hooks

---
+++ exoflex/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['@typescript-eslint', 'react'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+  ],
+  rules: {
+    indent: 'off',
+    '@typescript-eslint/prefer-interface': 0,
+    '@typescript-eslint/explicit-member-accessibility': 0,
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/no-use-before-define': ['error', {variables: false}],
+    '@typescript-eslint/no-unused-vars': ['error', {argsIgnorePattern: '^_'}],
+    '@typescript-eslint/array-type': ['error', 'generic']
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
