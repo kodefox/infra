eb3a92faba1623ebd11767f942ea7bb7e66f954a
Yoan Pratama Putra <yoanpratamaputra@gmail.com>
2019-08-16 14:59:31 +0700

    Implement naviflex on every screens

--- naviflex/example/src/screens/Third.tsx
+++ naviflex/example/src/screens/Third.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import {View, Text, Button} from 'react-native';
 
+import {useNavigation} from 'naviflex';
+
 export default function ThirdScreen() {
+  let {goBack} = useNavigation();
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>Third Screen</Text>
-      <Button title="Back to Second Screen" onPress={() => {}} />
+      <Button title="Back to Second Screen" onPress={() => goBack()} />
     </View>
   );
 }
