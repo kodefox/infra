0f4472cc312281d0e909753b39f83b6e4b5b9b19
Yosua Ian Sebastian <yosua.ian.sebastian@gmail.com>
2019-08-19 14:31:37 +0700

    add typing for fonts

--- packages/exoflex/src/types.ts
+++ packages/exoflex/src/types.ts
@@ -1,3 +1,30 @@
 import { ThemeShape } from 'react-native-paper';
 
-export type Theme = Omit<ThemeShape, 'fonts'>;
+export type Theme = Omit<ThemeShape, 'fonts'> & {
+  fonts?: Fonts;
+};
+
+type FontWeight =
+  | '100'
+  | '200'
+  | '300'
+  | '400'
+  | '500'
+  | '600'
+  | '700'
+  | '800'
+  | '900'
+  | 'regular'
+  | 'normal'
+  | 'bold'
+  | 'light';
+
+type FontStyle = 'normal' | 'italic';
+
+type Font = {
+  file: string;
+  weight: FontWeight;
+  style: FontStyle;
+};
+
+export type Fonts = { [fontFamily: string]: Array<Font> };
