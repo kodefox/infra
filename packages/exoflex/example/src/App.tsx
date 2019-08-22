71c51b57cb7a1de4f7a24edd777cb1633e969177
Yosua Ian Sebastian <yosua.ian.sebastian@gmail.com>
2019-08-22 17:02:55 +0700

    add Text and Provider example usage

--- packages/exoflex/example/src/App.tsx
+++ packages/exoflex/example/src/App.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, ActivityIndicator } from 'react-native';
+import { loadAsync } from 'expo-font';
+import { Text, Provider, DefaultTheme, BuiltInFonts } from 'exoflex';
 
 export default function App() {
+  let [isFontLoaded, setFontLoaded] = useState(false);
+
+  useEffect(() => {
+    loadAsync(BuiltInFonts).then(() => {
+      setFontLoaded(true);
+    });
+  }, []);
+
+  if (!isFontLoaded) {
+    return <ActivityIndicator />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Exoflex</Text>
-    </View>
+    <Provider theme={DefaultTheme}>
+      <View style={styles.container}>
+        <Text weight="700">Exoflex</Text>
+        <Text fontStyle="italic">Cool</Text>
+      </View>
+    </Provider>
   );
 }
 
