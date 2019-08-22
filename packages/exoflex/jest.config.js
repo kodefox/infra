c4cc426a97d3eb09fab335c95e931fd7da12e7fb
Yoan Pratama Putra <yoanpratamaputra@gmail.com>
2019-08-22 15:01:50 +0700

    Make jest ignore transforming a few libraries

--- packages/exoflex/jest.config.js
+++ packages/exoflex/jest.config.js
@@ -13,4 +13,7 @@ module.exports = {
     },
   },
   modulePathIgnorePatterns: ['<rootDir>/lib/'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|react-native-paper|react-native-safe-area-view)/)'
+  ],
 };
