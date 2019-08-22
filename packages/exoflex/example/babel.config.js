5ca3bbf1c9540e59e00d9fb18d2759ff873a98f5
Yosua Ian Sebastian <yosua.ian.sebastian@gmail.com>
2019-08-22 16:42:21 +0700

    configure example project to use local exoflex file

--- packages/exoflex/example/babel.config.js
+++ packages/exoflex/example/babel.config.js
@@ -1,6 +1,17 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ['expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            exoflex: '../src/index',
+          },
+        },
+      ],
+    ],
   };
 };
