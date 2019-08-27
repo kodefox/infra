7a3681ac26348eead0b8b52f0fda85a61c30fb37
Nathania Elvina <nathaniaelvina@hotmail.com>
2019-08-27 16:47:59 +0700

    exoflex: fix example project config (#26)
    * fix example project config

--- packages/exoflex/src/constants/fonts.ts
+++ packages/exoflex/src/constants/fonts.ts
@@ -1,11 +1,11 @@
-import RubikBold from '../../assets/fonts/Rubik-Bold.ttf';
-import RubikBoldItalic from '../../assets/fonts/Rubik-BoldItalic.ttf';
-import RubikLight from '../../assets/fonts/Rubik-Light.ttf';
-import RubikLightItalic from '../../assets/fonts/Rubik-LightItalic.ttf';
-import RubikMedium from '../../assets/fonts/Rubik-Medium.ttf';
-import RubikMediumItalic from '../../assets/fonts/Rubik-MediumItalic.ttf';
-import RubikRegular from '../../assets/fonts/Rubik-Regular.ttf';
-import RubikRegularItalic from '../../assets/fonts/Rubik-RegularItalic.ttf';
+import RubikBold from '../assets/fonts/Rubik-Bold.ttf';
+import RubikBoldItalic from '../assets/fonts/Rubik-BoldItalic.ttf';
+import RubikLight from '../assets/fonts/Rubik-Light.ttf';
+import RubikLightItalic from '../assets/fonts/Rubik-LightItalic.ttf';
+import RubikMedium from '../assets/fonts/Rubik-Medium.ttf';
+import RubikMediumItalic from '../assets/fonts/Rubik-MediumItalic.ttf';
+import RubikRegular from '../assets/fonts/Rubik-Regular.ttf';
+import RubikRegularItalic from '../assets/fonts/Rubik-RegularItalic.ttf';
 
 import { FontWeight, FontPreset } from '../types';
 
