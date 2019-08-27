0f338436a8c5cd0db63fd3555a62d40baf6be32f
Joshua <joshuasetiawan04@gmail.com>
2019-08-27 13:36:56 +0700

    setup

---
+++ packages/flexship/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    indent: 'off',
+    'prefer-const': 'off',
+    '@typescript-eslint/prefer-interface': 0,
+    '@typescript-eslint/explicit-member-accessibility': 0,
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/no-use-before-define': ['error', { variables: false }],
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/array-type': ['error', { default: 'generic' }],
+  },
+};
