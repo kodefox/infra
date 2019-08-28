a0448a6e931cf56db148cfa8b565a50bcd0ef3ac
Yosua Ian Sebastian <yosua.ian.sebastian@gmail.com>
2019-08-28 14:56:33 +0700

    reapply #22

--- packages/exoflex/example/babel.config.js
+++ packages/exoflex/example/babel.config.js
@@ -2,5 +2,16 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            exoflex: '../src/index',
+          },
+        },
+      ],
+    ],
   };
 };
