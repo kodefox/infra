e89a6218472c29480fbf770cd435df0da4f436e9
Joshua Setiawan <joshuasetiawan04@gmail.com>
2019-08-28 12:06:50 +0700

    flexship: initialize cli (#25)
    * setup
    
    * add init command
    
    * add js to included commands extension
    
    * apply suggestions
    
    * change watch script and change echo to git

---
+++ packages/flexship/src/commands/init.ts
@@ -0,0 +1,50 @@
+import inquirer from 'inquirer';
+import { spawn } from 'child_process';
+export let command = 'init';
+export let desc = 'Create a new project';
+
+type ProjectType = 'Expo (Frontend)' | 'Express (Backend)';
+type Answers = {
+  projectType: ProjectType;
+  projectName: string;
+};
+let REPO: Record<ProjectType, string> = {
+  'Expo (Frontend)': 'https://github.com/kodefox/example-expo-ts.git',
+  'Express (Backend)': 'https://github.com/kodefox/example-express-ts.git',
+};
+
+export let handler = async () => {
+  let { projectType, projectName }: Answers = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'projectType',
+      message: 'What project you want to create?',
+      choices: Object.keys(REPO),
+    },
+    {
+      type: 'input',
+      name: 'projectName',
+      message: 'What is the project name? ex:my-cool-project',
+      validate: (answer: string) => {
+        if (answer.trim() === '') {
+          return `project name can't be empty`;
+        }
+        return true;
+      },
+    },
+  ]);
+
+  let git = spawn('git', ['clone', REPO[projectType], projectName.trim()]);
+
+  git.stderr.on('data', (data) => {
+    console.log(`${data}`);
+  });
+
+  git.on('close', (code) => {
+    if (code !== 0) {
+      console.log(`Something went wrong, git exited with code ${code}`);
+      return;
+    }
+    console.log('Finish cloning the repo');
+  });
+};
