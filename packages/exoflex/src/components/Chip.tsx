fabf204ad887086b3090d4d71a536f62b25a9890
Yosua Ian Sebastian <yosua.ian.sebastian@gmail.com>
2019-08-28 11:41:27 +0700

    add Chip component

---
+++ packages/exoflex/src/components/Chip.tsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import {
+  TouchableOpacity,
+  StyleSheet,
+  TouchableOpacityProps,
+  StyleProp,
+  TextStyle,
+} from 'react-native';
+
+import { useTheme } from './Provider';
+import Text from './Text';
+
+type ModeProps = 'active' | 'inactive';
+
+type Props = TouchableOpacityProps & {
+  mode: ModeProps;
+  children?: string;
+  textStyle?: StyleProp<TextStyle>;
+};
+
+function Chip({
+  mode,
+  children,
+  onPress,
+  style,
+  textStyle,
+  ...otherProps
+}: Props) {
+  let { colors } = useTheme();
+
+  let isActive = mode === 'active';
+
+  return (
+    <TouchableOpacity
+      activeOpacity={0.7}
+      disabled={!onPress}
+      style={[
+        styles.root,
+        isActive
+          ? {
+              backgroundColor: colors.primary,
+            }
+          : {
+              borderColor: colors.primary,
+              borderWidth: 1,
+              backgroundColor: colors.surface,
+            },
+        style,
+      ]}
+      {...otherProps}
+    >
+      <Text
+        style={[
+          {
+            color: isActive ? colors.surface : colors.primary,
+          },
+          textStyle,
+        ]}
+      >
+        {children}
+      </Text>
+    </TouchableOpacity>
+  );
+}
+
+Chip.defaultProps = {
+  mode: 'active',
+};
+
+let styles = StyleSheet.create({
+  root: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: 36,
+    paddingHorizontal: 16,
+    borderRadius: 18,
+  },
+});
+
+export default Chip;
--- packages/exoflex/src/components/index.ts
+++ packages/exoflex/src/components/index.ts
@@ -1,2 +1,3 @@
+export { default as Chip } from './Chip';
 export { default as Provider } from './Provider';
 export { default as Text } from './Text';
