f6509c559ea3a048d4280ae001eae3237a9c4595
Yosua Ian Sebastian <yosua.ian.sebastian@gmail.com>
2019-08-30 12:27:21 +0700

    add ToastContainer component

---
+++ packages/exoflex/src/components/ToastContainer.tsx
@@ -0,0 +1,71 @@
+import React, { useState, useEffect, useCallback, useRef } from 'react';
+
+import Toast, { ModeProps } from './Toast';
+import EventEmitter from '../helpers/EventEmitter';
+
+export type ToastConfig = {
+  message: string;
+  mode?: ModeProps;
+  duration?: number;
+};
+
+type EmitEvents = { showToast: ToastConfig; hideToast: undefined };
+
+let DefaultToastConfig = {
+  message: '',
+  mode: 'info' as ModeProps,
+  duration: 4000,
+};
+
+let ToastEmitter = EventEmitter.create<EmitEvents>();
+
+function showToast(config: ToastConfig) {
+  ToastEmitter.emit('showToast', config);
+}
+
+function hideToast() {
+  ToastEmitter.emit('hideToast', undefined);
+}
+
+function ToastContainer() {
+  let [visible, setVisible] = useState(false);
+  let [config, setConfig] = useState(DefaultToastConfig);
+
+  let timerID = useRef<number | undefined>();
+
+  let onShowToast = useCallback((newConfig: ToastConfig) => {
+    setVisible(true);
+    setConfig({ ...DefaultToastConfig, ...newConfig });
+  }, []);
+  let onHideToast = useCallback(() => {
+    setVisible(false);
+  }, []);
+
+  useEffect(() => {
+    if (visible) {
+      timerID.current = setTimeout(hideToast, config.duration);
+    }
+    return () => clearTimeout(timerID.current);
+  }, [visible]);
+
+  useEffect(() => {
+    ToastEmitter.on('showToast', onShowToast);
+    ToastEmitter.on('hideToast', onHideToast);
+    return () => {
+      ToastEmitter.remove('showToast', onShowToast);
+      ToastEmitter.remove('hideToast', onHideToast);
+    };
+  }, []);
+
+  return (
+    <Toast visible={visible} mode={config.mode}>
+      {config.message}
+    </Toast>
+  );
+}
+
+Toast.showToast = showToast;
+Toast.hideToast = hideToast;
+
+export { ToastEmitter, showToast, hideToast };
+export default ToastContainer;
