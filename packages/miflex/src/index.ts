ee784ded33836a8e11483d6473331651cedb4515
Yosua Ian Sebastian <yosua.ian.sebastian@gmail.com>
2019-08-30 15:54:56 +0700

    add EventEmitter and test

---
+++ packages/miflex/src/index.ts
@@ -0,0 +1,44 @@
+type EventHandler<T> = (event: T) => void;
+type EventHandlerList<T> = Set<EventHandler<T>> | undefined;
+type EventSubscription = { unsubscribe: () => void };
+
+class EventEmitter<T extends {}, K extends keyof T> {
+  static create<T>() {
+    return new EventEmitter<T, keyof T>();
+  }
+
+  _events: Map<K, any> = new Map();
+
+  on = <U extends K>(
+    eventName: U,
+    handler: EventHandler<T[U]>,
+  ): EventSubscription => {
+    let eventHandlerList: EventHandlerList<T[U]> = this._events.get(eventName);
+    if (!eventHandlerList) {
+      eventHandlerList = new Set();
+      this._events.set(eventName, eventHandlerList);
+    }
+    eventHandlerList.add(handler);
+    return {
+      unsubscribe: () => this.remove(eventName, handler),
+    };
+  };
+
+  emit = <U extends K>(eventName: U, eventData: T[U]) => {
+    let eventHandlerList: EventHandlerList<T[U]> = this._events.get(eventName);
+    if (eventHandlerList) {
+      for (let handler of eventHandlerList) {
+        handler(eventData);
+      }
+    }
+  };
+
+  remove = <U extends K>(eventName: U, handler: EventHandler<T[U]>) => {
+    let eventHandlerList = this._events.get(eventName);
+    if (eventHandlerList) {
+      eventHandlerList.delete(handler);
+    }
+  };
+}
+
+export default EventEmitter;
