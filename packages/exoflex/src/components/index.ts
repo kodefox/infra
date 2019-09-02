bc7c8cbd3e2689a6742b76530493c5c7b8be08c6
Yosua Ian Sebastian <yosua.ian.sebastian@gmail.com>
2019-09-02 12:47:06 +0700

    add activity indicator component

--- packages/exoflex/src/components/index.ts
+++ packages/exoflex/src/components/index.ts
@@ -1,3 +1,4 @@
+export { default as ActivityIndicator } from './ActivityIndicator';
 export { default as Chip } from './Chip';
 export { default as Provider } from './Provider';
 export { default as Switch } from './Switch';
