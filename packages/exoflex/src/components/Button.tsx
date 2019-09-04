aa72c587b669dadf53f8735baedf95c8c958b2c1
Yoan Pratama Putra <yoanpratamaputra@gmail.com>
2019-09-04 16:18:27 +0700

    Styling button

--- packages/exoflex/src/components/Button.tsx
+++ packages/exoflex/src/components/Button.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { StyleSheet } from 'react-native';
 import { Button as PaperButton, ButtonProps } from 'react-native-paper';
 import Text from './Text';
 
-// import { useTheme } from './Provider';
+import { useTheme } from './Provider';
 
 type ButtonPresets = {
   primary: 'contained';
@@ -22,20 +23,38 @@ const PRESETS: ButtonPresets = {
 };
 
 export default function Button(props: Props) {
-  let { preset, children, uppercase, ...buttonProps } = props;
+  let {
+    preset,
+    children,
+    uppercase,
+    contentStyle,
+    style,
+    ...buttonProps
+  } = props;
+  let { colors } = useTheme();
   let mode = PRESETS[preset];
 
-  let renderChildren = children;
-  if (typeof children === 'string') {
-    renderChildren = (
-      <Text weight="500">{uppercase ? children.toUpperCase() : children}</Text>
-    );
-  }
-
-  // TODO: Use combined/merged theme
   return (
-    <PaperButton mode={mode} uppercase={uppercase} {...buttonProps}>
-      {renderChildren}
+    <PaperButton
+      mode={mode}
+      uppercase={uppercase}
+      contentStyle={[styles.contentWrapper, contentStyle]}
+      style={[
+        preset === 'secondary' && {
+          borderWidth: 2,
+          borderColor: colors.primary,
+        },
+        style,
+      ]}
+      {...buttonProps}
+    >
+      {typeof children === 'string' ? (
+        <Text weight="500" style={{ fontSize: 14 }}>
+          {uppercase ? children.toUpperCase() : children}
+        </Text>
+      ) : (
+        children
+      )}
     </PaperButton>
   );
 }
@@ -44,3 +63,10 @@ Button.defaultProps = {
   preset: 'primary',
   uppercase: true,
 };
+
+const styles = StyleSheet.create({
+  contentWrapper: {
+    padding: 16,
+    minWidth: 158,
+  },
+});
