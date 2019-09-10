c2b204cff848871b46cbab30ea740e774111cd80
nathaniaelvina <nathaniaelvina@hotmail.com>
2019-09-10 16:25:56 +0700

    fix rn svg error

--- packages/exoflex/example/webpack.config.js
+++ packages/exoflex/example/webpack.config.js
@@ -37,6 +37,9 @@ module.exports = async function(env, argv) {
   config.resolve.alias['react-native-web/dist/exports'] = path.resolve(
     './node_modules/react-native-web/dist/exports',
   );
+  config.resolve.alias['react-native-svg'] = path.resolve(
+    './node_modules/react-native-svg',
+  );
 
   // Add rule to transform exoflex files before loading it.
   config.module.rules = [
