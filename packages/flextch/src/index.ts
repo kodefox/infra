02af977bd90ba068c709cf1092fe572adffe90fb
Yoan Pratama Putra <yoanpratamaputra@gmail.com>
2019-09-28 17:50:51 +0700

    Create useQuery hooks

---
+++ packages/flextch/src/index.ts
@@ -0,0 +1,28 @@
+import { useQuery as useRFLQuery, Action } from 'react-fetching-library';
+import { isLeft } from 'fp-ts/lib/Either';
+import * as iots from 'io-ts';
+
+// Re-export all io-ts modules
+export { iots };
+
+export function useQuery<T = any, R = {}>(
+  action: Action<R>,
+  validator: iots.Type<T>,
+  initFetch?: boolean,
+) {
+  let result = useRFLQuery<T>(action, initFetch);
+  let { payload, error } = result;
+  if (error) {
+    return result;
+  }
+  if (!payload) {
+    return result;
+  }
+  let validatedPayload = validator.decode(payload);
+  if (isLeft(validatedPayload)) {
+    // TODO: Return error info
+    return { ...result, error: true };
+  }
+  let newPayload: iots.TypeOf<typeof validator> = payload;
+  return { ...result, error: false, payload: newPayload };
+}
