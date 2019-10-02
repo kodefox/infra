dcee2fff3e2ede898ce603bedd7bcb77e55342a8
Joshua <joshuasetiawan04@gmail.com>
2019-10-02 17:17:00 +0700

    update useQuery data handling

--- packages/flextch/src/hooks/useQuery.ts
+++ packages/flextch/src/hooks/useQuery.ts
@@ -1,45 +1,35 @@
-import {
-  useQuery as useLibraryQuery,
-  Action,
-  QueryResponse,
-} from 'react-fetching-library';
+import { useQuery as useLibraryQuery, Action } from 'react-fetching-library';
 
 import { Type } from 'io-ts';
 import { isLeft } from 'fp-ts/lib/Either';
 
 type QueryOption<T> = {
-  initFetch: boolean;
+  initFetch?: boolean;
   schema: Type<T>;
 };
 
-//this is a type extracted from useQuery result
-type UseQueryResponse<T> = {
-  status?: number | undefined;
-  error: boolean;
-  errorObject?: any;
-  payload?: T | undefined;
-  headers?: Headers | undefined;
-  abort: () => void;
-  loading: boolean;
-  query: () => Promise<QueryResponse<T>>;
-  reset: () => void;
-};
+type UseQueryResponse<T> = Omit<
+  ReturnType<typeof useLibraryQuery>,
+  'payload'
+> & { payload?: T };
 
 function useQuery<T>(
   action: Action,
-  { schema, initFetch = false }: QueryOption<T>,
+  option: QueryOption<T>,
 ): UseQueryResponse<T> {
-  let { payload, loading, ...queryResult } = useLibraryQuery(action, initFetch);
+  let { payload, ...queryResult } = useLibraryQuery(action, option.initFetch);
   //loading
-  if (loading) {
+  if (queryResult.loading) {
     return { ...queryResult, loading: true };
   }
-  let decodedPayload = schema.decode(payload);
+  if (queryResult.error) {
+    return { ...queryResult };
+  }
+  let decodedPayload = option.schema.decode(payload);
   //if decode result in error
   if (isLeft(decodedPayload)) {
     return {
       ...queryResult,
-      loading: false,
       error: true,
       errorObject: decodedPayload.left,
     };
@@ -47,8 +37,7 @@ function useQuery<T>(
   //successfull decode
   return {
     ...queryResult,
-    loading: false,
-    payload: decodedPayload.right,
+    payload: payload,
   };
 }
 
