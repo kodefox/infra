aef562a19bdc0b510337d82817e1e3a730239c9c
Yoan Pratama Putra <yoanpratamaputra@gmail.com>
2019-10-05 17:11:21 +0700

    feat(exoflex): create timeChecker helper

---
+++ packages/exoflex/src/helpers/timeChecker.ts
@@ -0,0 +1,21 @@
+const HOUR_FORMAT_12 = /^([1][0-2]|[0][1-9])$/;
+const HOUR_FORMAT_24 = /^([2][0-3]|[1][0-9]|[0][0-9])$/;
+const MINUTE_FORMAT = /^([0-5][0-9])$/;
+const SECOND_FORMAT = /^([0-5][0-9])$/;
+
+export type HourFormat = '12' | '24';
+export type Midnight = 'am' | 'pm';
+
+export function checkHourFormat(hour: string, format: HourFormat = '12') {
+  return format === '12'
+    ? HOUR_FORMAT_12.test(hour)
+    : HOUR_FORMAT_24.test(hour);
+}
+
+export function checkMinuteFormat(minute: string) {
+  return MINUTE_FORMAT.test(minute);
+}
+
+export function checkSecondFormat(second: string) {
+  return SECOND_FORMAT.test(second);
+}
