9eae630059d52bdcfc59a12454c67f1e3943ee64
Kevin Lie <Me@Kevin.local>
2019-10-07 17:35:24 +0700

    Add and fix global file for testing typescript file

--- packages/flexship/src/type.ts
+++ packages/flexship/src/type.ts
@@ -1,5 +1,9 @@
+import fetch from 'node-fetch';
 export type ProjectType = 'Expo (Frontend)' | 'Express (Backend)' | 'Node';
 export type Answers = {
   projectType: ProjectType;
   projectName: string;
 };
+export type MockFetch = (url: RequestInfo, init?: RequestInit) => Promise<any>;
+export type RealFetch = typeof fetch;
+export type Fetch = RealFetch | MockFetch;
