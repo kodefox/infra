f4e1564d9a8ac65fec2367ddde7447dd2a9d366e
Yoan Pratama Putra <yoanpratamaputra@gmail.com>
2019-10-14 17:07:50 +0700

    chore: fix git push does not respect pre-push script

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -4,5 +4,8 @@ for package in packages/*; do
   if [[ $(git diff master..HEAD --name-only | grep $package) != "" ]]; then
     cd $package
     yarn test
+    if [[ $? != 0 ]]; then
+      exit 1
+    fi
   fi
 done
