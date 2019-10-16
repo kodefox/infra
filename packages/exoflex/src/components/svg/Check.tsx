c16b5bc97a7a2dc7a038382f80d11e21c60a158b
Yosua Ian Sebastian <yosua.ian.sebastian@gmail.com>
2019-10-16 17:23:34 +0700

    fix(exoflex): fix invalid property passed to DOM (#139)

--- packages/exoflex/src/components/svg/Check.tsx
+++ packages/exoflex/src/components/svg/Check.tsx
@@ -13,7 +13,7 @@ let Check = (props: IconProps) => {
       <Path
         d="M13.28 20.72a1.328 1.328 0 0 1-1.88 0l-4.787-4.787a1.329 1.329 0 0 1 1.88-1.88l3.84 3.84 9.174-9.173a1.329 1.329 0 1 1 1.88 1.88L13.28 20.72z"
         fill={props.fill || DefaultTheme.colors.placeholder}
-        fill-rule="nonzero"
+        fillRule="nonzero"
       />
       <Path d="M0 0h17.55v17.55H0z" fill="transparent" />
     </Svg>
