3491370d4a0b451c47ac5ddf71b81e3ae864dd42
nathaniaelvina <nathaniaelvina@hotmail.com>
2019-10-18 12:07:41 +0700

    chore(exofle): export examples

--- packages/exoflex/example/src/examples/index.tsx
+++ packages/exoflex/example/src/examples/index.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/display-name */
 import React from 'react';
 
+import ActivityIndicatorExample from './ActivityIndicatorExample';
 import ButtonExample from './ButtonExample';
 import CardExample from './CardExample';
 import CalendarExample from './CalendarExample';
@@ -9,12 +10,16 @@ import CollapsibleExample from './CollapsibleExample';
 import SliderExample from './SliderExample';
 import IconButtonExample from './IconButtonExample';
 import ProgressBarExample from './ProgressBarExample';
+import RadioButtonExample from './RadioButtonExample';
+import RadioButtonGroupExample from './RadioButtonGroupExample';
+import SwitchExample from './SwitchExample';
 import TextInputExample from './TextInputExample';
 import ToastExample from './ToastExample';
 
 import Welcome from '../Welcome';
 
 export let EXAMPLES = {
+  activityindicator: ActivityIndicatorExample,
   button: ButtonExample,
   card: CardExample,
   calendar: CalendarExample,
@@ -22,7 +27,10 @@ export let EXAMPLES = {
   collapsible: CollapsibleExample,
   iconbutton: IconButtonExample,
   progressbar: ProgressBarExample,
+  radiobutton: RadioButtonExample,
+  radiobuttongroup: RadioButtonGroupExample,
   slider: SliderExample,
+  switch: SwitchExample,
   textinput: TextInputExample,
   toast: ToastExample,
 };
