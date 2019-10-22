c8fd04fdcc2761f9579bd7977309658be484d9cc
Yoan Pratama Putra <yoanpratamaputra@gmail.com>
2019-10-22 17:03:31 +0700

    fix(exoflex): bug on convertTimeToDate

--- packages/exoflex/src/helpers/__tests__/resolveTime.test.ts
+++ packages/exoflex/src/helpers/__tests__/resolveTime.test.ts
@@ -6,44 +6,50 @@ const initialDate = date.toISOString();
 
 describe('convertTimeToDate', () => {
   it('should return valid date from 24h format', () => {
-    expect(convertTimeToDate(initialDate, '12', '00', '00').split('.')[0]).toBe(
-      new Date(date.setHours(12, 0, 0)).toISOString().split('.')[0],
+    expect(convertTimeToDate(initialDate, '12', '00', '00')).toBe(
+      new Date(date.setHours(12, 0, 0)).toISOString(),
     );
-    expect(convertTimeToDate(initialDate, '01', '00', '00').split('.')[0]).toBe(
-      new Date(date.setHours(1, 0, 0)).toISOString().split('.')[0],
+    expect(convertTimeToDate(initialDate, '01', '00', '00')).toBe(
+      new Date(date.setHours(1, 0, 0)).toISOString(),
     );
-    expect(convertTimeToDate(initialDate, '23', '59', '01').split('.')[0]).toBe(
-      new Date(date.setHours(23, 59, 1)).toISOString().split('.')[0],
+    expect(convertTimeToDate(initialDate, '23', '59', '01')).toBe(
+      new Date(date.setHours(23, 59, 1)).toISOString(),
     );
-    expect(convertTimeToDate(initialDate, '12', '00', '00').split('T')[0]).toBe(
-      new Date(date.setHours(12, 0, 0)).toISOString().split('T')[0],
+    expect(convertTimeToDate(initialDate, '12', '00', '00')).toBe(
+      new Date(date.setHours(12, 0, 0)).toISOString(),
     );
-    expect(convertTimeToDate(initialDate, '01', '00', '00').split('T')[0]).toBe(
-      new Date(date.setHours(1, 0, 0)).toISOString().split('T')[0],
+    expect(convertTimeToDate(initialDate, '01', '00', '00')).toBe(
+      new Date(date.setHours(1, 0, 0)).toISOString(),
     );
-    expect(convertTimeToDate(initialDate, '23', '59', '01').split('T')[0]).toBe(
-      new Date(date.setHours(23, 59, 1)).toISOString().split('T')[0],
+    expect(convertTimeToDate(initialDate, '23', '59', '01')).toBe(
+      new Date(date.setHours(23, 59, 1)).toISOString(),
     );
   });
 
   it('should return valid date from 12h format', () => {
-    expect(
-      convertTimeToDate(initialDate, '11', '59', '01', 'PM').split('.')[0],
-    ).toBe(new Date(date.setHours(23, 59, 1)).toISOString().split('.')[0]);
-    expect(
-      convertTimeToDate(initialDate, '11', '59', '01', 'AM').split('.')[0],
-    ).toBe(new Date(date.setHours(11, 59, 1)).toISOString().split('.')[0]);
-    expect(
-      convertTimeToDate(initialDate, '11', '59', '01', 'PM').split('T')[0],
-    ).toBe(new Date(date.setHours(23, 59, 1)).toISOString().split('T')[0]);
-    expect(
-      convertTimeToDate(initialDate, '11', '59', '01', 'AM').split('T')[0],
-    ).toBe(new Date(date.setHours(11, 59, 1)).toISOString().split('T')[0]);
+    expect(convertTimeToDate(initialDate, '12', '00', '00', 'PM')).toBe(
+      new Date(date.setHours(12, 0, 0)).toISOString(),
+    );
+    expect(convertTimeToDate(initialDate, '12', '00', '00', 'AM')).toBe(
+      new Date(date.setHours(0, 0, 0)).toISOString(),
+    );
+    expect(convertTimeToDate(initialDate, '11', '59', '01', 'PM')).toBe(
+      new Date(date.setHours(23, 59, 1)).toISOString(),
+    );
+    expect(convertTimeToDate(initialDate, '11', '59', '01', 'AM')).toBe(
+      new Date(date.setHours(11, 59, 1)).toISOString(),
+    );
+    expect(convertTimeToDate(initialDate, '11', '59', '01', 'PM')).toBe(
+      new Date(date.setHours(23, 59, 1)).toISOString(),
+    );
+    expect(convertTimeToDate(initialDate, '11', '59', '01', 'AM')).toBe(
+      new Date(date.setHours(11, 59, 1)).toISOString(),
+    );
   });
 });
 
 describe('convertDateToLocalTime', () => {
-  const date1 = new Date('2019-10-10T11:03:11.000Z');
+  const date1 = new Date('2019-10-10T05:03:11.000Z');
   const date2 = new Date('2019-10-10T00:03:11.000Z');
 
   it('should return array of time for 24h format', () => {
