5fb56629a3e3322cfb1fa537baa4281d373ceea2
Nathania Elvina <nathaniaelvina@hotmail.com>
2019-10-31 10:48:48 +0700

    feat(exoflex): add Portal component (#160)
    * feat(exoflex): export Portal Component
    
    * docs(exoflex): add Portal example

---
+++ packages/exoflex/example/src/examples/PortalExample.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Portal, Text } from '../../../src/components';
+
+function PortalExample() {
+  return (
+    <>
+      <Portal>
+        <View style={styles.portalContainer}>
+          <Text style={styles.text}>Hello</Text>
+        </View>
+      </Portal>
+      <View style={styles.root}>
+        <Text>This is the scene content</Text>
+      </View>
+    </>
+  );
+}
+
+let styles = StyleSheet.create({
+  portalContainer: {
+    flex: 1,
+    backgroundColor: '#0f0f0f',
+    opacity: 0.7,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: { color: 'tomato' },
+  root: { padding: 20, alignItems: 'center' },
+});
+
+PortalExample.title = 'Portal';
+
+export default PortalExample;
