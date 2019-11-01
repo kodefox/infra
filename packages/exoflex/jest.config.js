33bd8a24f4d71acd8f125b9fb3884a6166c9fd5d
Yoan Pratama Putra <yoanpratamaputra@gmail.com>
2019-11-01 17:37:24 +0700

    test(exoflex): remove ts-jest; add @babel/typescript, patch-package

--- packages/exoflex/jest.config.js
+++ packages/exoflex/jest.config.js
@@ -1,18 +1,6 @@
-const { defaults: tsjPreset } = require('ts-jest/presets');
-
 module.exports = {
-  ...tsjPreset,
   setupFiles: ['<rootDir>/test/modules-mock'],
   preset: './jestPresets/jest-preset',
-  transform: {
-    ...tsjPreset.transform,
-    '\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js',
-  },
-  globals: {
-    'ts-jest': {
-      babelConfig: true,
-    },
-  },
   modulePathIgnorePatterns: ['<rootDir>/lib/', '<rootDir>/example/'],
   transformIgnorePatterns: [
     'node_modules/(?!(react-native|react-native-paper|react-native-safe-area-view|react-native-animation-hooks|react-native-svg|react-native-calendars|react-native-multi-slider|react-native-collapsible|react-native-modal-datetime-picker|react-native-vector-icons)/)',
