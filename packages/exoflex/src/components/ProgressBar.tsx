7bfddaea29b3977371e8566bfba59c223106f718
nathaniaelvina <nathaniaelvina@hotmail.com>
2019-11-04 13:39:16 +0700

    feat(exoflex): enable indeterminate prop

--- packages/exoflex/src/components/ProgressBar.tsx
+++ packages/exoflex/src/components/ProgressBar.tsx
@@ -1,24 +1,50 @@
-import React from 'react';
-import { StyleSheet, View, Animated, ViewStyle } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  StyleSheet,
+  View,
+  Animated,
+  ViewStyle,
+  Platform,
+  LayoutChangeEvent,
+} from 'react-native';
 import { ProgressBarProps } from 'react-native-paper';
-import { useAnimation } from 'react-native-animation-hooks';
 import useTheme from '../helpers/useTheme';
 
-// TODO: allow `indeterminate` prop after upgrading to RNP 3
-type Props = Omit<ProgressBarProps, 'indeterminate' | 'animating' | 'theme'> & {
+type Props = Omit<ProgressBarProps, 'animating' | 'theme'> & {
   visible?: boolean;
 };
 
+const INDETERMINATE_MAX_WIDTH = 0.6;
+
 export default function ProgressBar(props: Props) {
-  let { progress, color, visible, style } = props;
+  let { progress, color, visible, style, indeterminate } = props;
+  let [animatedValue] = useState(new Animated.Value(0));
+  let [width, setWidth] = useState(0);
   let { colors, roundness } = useTheme();
 
-  let animatedValue = useAnimation({
-    type: 'timing',
-    initialValue: 0,
-    toValue: 1,
-    duration: 500,
-  });
+  useEffect(() => {
+    startAnimation();
+  }, [progress]);
+
+  let startAnimation = () => {
+    if (indeterminate) {
+      let indeterminateAnimation = Animated.timing(animatedValue, {
+        duration: 1000,
+        toValue: 1,
+        // Animated.loop does not work if useNativeDriver is true on web
+        useNativeDriver: Platform.OS !== 'web',
+        isInteraction: false,
+      });
+      Animated.loop(indeterminateAnimation).start();
+    } else {
+      Animated.timing(animatedValue, {
+        duration: 500,
+        toValue: progress ? progress : 0,
+        useNativeDriver: true,
+        isInteraction: false,
+      }).start();
+    }
+  };
 
   let flattenedStyle = StyleSheet.flatten<ViewStyle>(style) || {};
   let height = flattenedStyle.height || 8;
@@ -40,6 +66,7 @@ export default function ProgressBar(props: Props) {
         },
         style,
       ]}
+      onLayout={(e: LayoutChangeEvent) => setWidth(e.nativeEvent.layout.width)}
     >
       <Animated.View
         style={[
@@ -47,10 +74,44 @@ export default function ProgressBar(props: Props) {
           {
             borderRadius,
             backgroundColor: color || colors.primary,
-            width: animatedValue.interpolate({
-              inputRange: [0, 1],
-              outputRange: ['0%', `${progress * 100}%`],
-            }),
+            width,
+            transform: [
+              {
+                translateX: animatedValue.interpolate(
+                  indeterminate
+                    ? {
+                        inputRange: [0, 0.5, 1],
+                        outputRange: [
+                          -0.5 * width,
+                          -0.5 * INDETERMINATE_MAX_WIDTH * width,
+                          0.7 * width,
+                        ],
+                      }
+                    : {
+                        inputRange: [0, 1],
+                        outputRange: [-0.5 * width, 0],
+                      },
+                ),
+              },
+              {
+                /**
+                 * From RNP code: Workaround for workaround for https://github.com/facebook/react-native/issues/6278
+                 *
+                 * I tried putting 0 instead 0.0001, it works on ios, android, and web. But perhaps this workaround will help on other case, I leave it as it is.
+                 */
+                scaleX: animatedValue.interpolate(
+                  indeterminate
+                    ? {
+                        inputRange: [0, 0.5, 1],
+                        outputRange: [0.0001, INDETERMINATE_MAX_WIDTH, 0.0001],
+                      }
+                    : {
+                        inputRange: [0, 1],
+                        outputRange: [0.0001, 1],
+                      },
+                ),
+              },
+            ],
           },
         ]}
       />
@@ -60,6 +121,7 @@ export default function ProgressBar(props: Props) {
 
 ProgressBar.defaultProps = {
   visible: true,
+  progress: 0,
 };
 
 const styles = StyleSheet.create({
