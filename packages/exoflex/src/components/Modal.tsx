60b096eba07fd2b19ae7d22d1197d48d76ae4c39
Yoan Pratama Putra <yoanpratamaputra@gmail.com>
2019-11-04 13:39:26 +0700

    fix(exoflex): omit theme prop from modal

--- packages/exoflex/src/components/Modal.tsx
+++ packages/exoflex/src/components/Modal.tsx
@@ -1,1 +1,11 @@
-export { Modal as default } from 'react-native-paper';
+import React from 'react';
+import {
+  Modal as PaperModal,
+  ModalProps as PaperModalProps,
+} from 'react-native-paper';
+
+type ModalProps = Omit<PaperModalProps, 'theme'>;
+
+export default function Modal(props: ModalProps) {
+  return <PaperModal {...props} />;
+}
