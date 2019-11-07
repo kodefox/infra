76a1d404a71cf0088c189edf9ac8133b0446464e
nathaniaelvina <nathaniaelvina@hotmail.com>
2019-11-07 16:31:10 +0700

    test(exoflex): add test for Segmented Control

---
+++ packages/exoflex/src/components/__tests__/SegmentedControl.test.native.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import Provider from '../Provider';
+import { render, fireEvent } from 'react-native-testing-library';
+import SegmentedControl from '../SegmentedControl';
+
+describe('SegmentedControl', () => {
+  it('should render default Segmented Control', () => {
+    let { getAllByText } = render(
+      <Provider>
+        <SegmentedControl activeIndex={0} values={['Tab One', 'Tab Two']} />
+      </Provider>,
+    );
+    expect(getAllByText(/\bTab/)).toBeTruthy();
+  });
+
+  it('should execute on segment change callback', () => {
+    let activeIndex = 0;
+    let changeIndex = jest.fn(() => {
+      activeIndex = 1;
+    });
+
+    let { getByText } = render(
+      <Provider>
+        <SegmentedControl
+          activeIndex={activeIndex}
+          values={['Tab One', 'Tab Two']}
+          onIndexChange={changeIndex}
+        />
+      </Provider>,
+    );
+    fireEvent.press(getByText('Tab Two'));
+    expect(changeIndex).toBeCalledTimes(1);
+    expect(activeIndex).toEqual(1);
+  });
+
+  it('should handle disabled Segmented Control', () => {
+    let activeIndex = 0;
+    let changeIndex = jest.fn(() => {
+      activeIndex = 1;
+    });
+
+    let { getByText } = render(
+      <Provider>
+        <SegmentedControl
+          activeIndex={activeIndex}
+          values={['Tab One', 'Tab Two']}
+          onIndexChange={changeIndex}
+          disabled={true}
+        />
+      </Provider>,
+    );
+    // NOTE: Disable this as this is still an issue (https://github.com/callstack/react-native-testing-library/issues/28)
+    // fireEvent.press(getByText('Tab Two'));
+    expect(getByText('Tab Two')).toBeTruthy();
+    expect(changeIndex).toBeCalledTimes(0);
+    expect(activeIndex).toEqual(0);
+  });
+});
---
+++ packages/exoflex/src/components/__tests__/SegmentedControl.test.web.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import Provider from '../Provider';
+import { render, fireEvent } from '@testing-library/react';
+import SegmentedControl from '../SegmentedControl';
+import '../../../test/customFireEvent.web';
+
+describe('SegmentedControl', () => {
+  it('should render default Segmented Control', () => {
+    let activeIndex = 0;
+    let changeIndex = jest.fn(() => {
+      activeIndex = 1;
+    });
+    let { getAllByText } = render(
+      <Provider>
+        <SegmentedControl
+          activeIndex={activeIndex}
+          values={['Tab One', 'Tab Two']}
+          onIndexChange={changeIndex}
+        />
+      </Provider>,
+    );
+    expect(getAllByText(/\bTab/)).toBeTruthy();
+  });
+
+  it('should execute on segment change callback', () => {
+    let activeIndex = 0;
+    let changeIndex = jest.fn(() => {
+      activeIndex = 1;
+    });
+
+    let { getByText } = render(
+      <Provider>
+        <SegmentedControl
+          activeIndex={activeIndex}
+          values={['Tab One', 'Tab Two']}
+          onIndexChange={changeIndex}
+        />
+      </Provider>,
+    );
+
+    fireEvent.click(getByText('Tab Two'));
+    expect(changeIndex).toBeCalledTimes(1);
+    expect(activeIndex).toEqual(1);
+  });
+
+  it('should handle disabled Segmented Control', () => {
+    let activeIndex = 0;
+    let changeIndex = jest.fn(() => {
+      activeIndex = 1;
+    });
+
+    let { getByText } = render(
+      <Provider>
+        <SegmentedControl
+          activeIndex={activeIndex}
+          values={['Tab One', 'Tab Two']}
+          onIndexChange={changeIndex}
+          disabled={true}
+        />
+      </Provider>,
+    );
+    fireEvent.click(getByText('Tab Two'));
+    expect(getByText('Tab Two')).toBeTruthy();
+    expect(changeIndex).toBeCalledTimes(0);
+    expect(activeIndex).toEqual(0);
+  });
+});
