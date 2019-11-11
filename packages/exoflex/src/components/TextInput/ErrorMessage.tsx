b5307e087676b8a918f3e4513e0a8f34d0f76d22
Yosua Ian Sebastian <yosua.ian.sebastian@gmail.com>
2019-11-11 14:10:47 +0700

    chore(exoflex): bump typescript to v3.7 (#191)

--- packages/exoflex/src/components/TextInput/ErrorMessage.tsx
+++ packages/exoflex/src/components/TextInput/ErrorMessage.tsx
@@ -3,13 +3,15 @@ import { StyleSheet } from 'react-native';
 
 import Label from '../Label';
 
-type Props = ComponentProps<Label>;
+type Props = ComponentProps<typeof Label>;
 
 function ErrorMessage({ style, ...otherProps }: Props) {
   return (
     <Label numberOfLines={1} {...otherProps} style={[styles.root, style]} />
   );
 }
+
+ErrorMessage.defaultProps = Label.defaultProps;
 
 let styles = StyleSheet.create({
   root: {
