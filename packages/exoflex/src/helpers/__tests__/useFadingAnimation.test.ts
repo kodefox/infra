kodefox/infra

commit dfb97f203a321b8946f006e939697ed44c14d5ab

fix(exoflex): make invisible Toast hidden from DOM (#188)

packages/exoflex/src/components/Toast.tsx
@@ -50,6 +50,11 @@ function Toast({ mode, visible, style, children, colors, textStyle }: Props) {
     useNativeDriver: Platform.OS !== 'web',
   });
 
+  if (!visible) {
+    // TODO: Reimplement Toast animation when entering and leaving.
+    return null;
+  }
+
   return (
     <SafeAreaView pointerEvents="box-none" style={styles.wrapper}>
       <Surface

commit 9c152826551f9576d8142d865d9c0a892f752b30

fix(exoflex): fix toast animation regression from #188 (#196)
* feat: add useFadingAnimation helper

* refactor: use useFadingAnimation in Toast

* test: add test for useFadingAnimation

* refactor: use const assertion instead of explicit return type

packages/exoflex/src/components/Toast.tsx
@@ -6,15 +6,14 @@ import {
   ViewStyle,
   View,
   TextStyle,
-  Platform,
 } from 'react-native';
 import { Surface, IconButton } from 'react-native-paper';
-import { useAnimation } from 'react-native-animation-hooks';
 
 import Text from './Text';
 import { ToastConfig } from './ToastContainer';
 import useTheme from '../helpers/useTheme';
 import { DefaultTheme } from '../constants/themes';
+import useFadingAnimation from '../helpers/useFadingAnimation';
 
 export type ModeProps = 'info' | 'warning' | 'error' | 'success';
 
@@ -41,17 +40,11 @@ type Props = {
 function Toast({ mode, visible, style, children, colors, textStyle }: Props) {
   let { colors: themeColors } = useTheme();
 
-  let animatedOpacity = useAnimation({
-    type: 'timing',
-    initialValue: visible ? 1 : 0,
-    toValue: visible ? 1 : 0,
+  let [animatedVisibility, animatedValue] = useFadingAnimation(visible, {
     duration: 200,
-    // Native driver is not available on web.
-    useNativeDriver: Platform.OS !== 'web',
   });
 
-  if (!visible) {
-    // TODO: Reimplement Toast animation when entering and leaving.
+  if (!animatedVisibility) {
     return null;
   }
 
@@ -65,15 +58,13 @@ function Toast({ mode, visible, style, children, colors, textStyle }: Props) {
             styles.container,
             {
               backgroundColor: colors[mode],
-              opacity: animatedOpacity,
+              opacity: animatedValue,
               transform: [
                 {
-                  scale: visible
-                    ? animatedOpacity.interpolate({
-                        inputRange: [0, 1],
-                        outputRange: [0.9, 1],
-                      })
-                    : 1,
+                  scale: animatedValue.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [0.8, 1],
+                  }),
                 },
               ],
             },

packages/exoflex/src/helpers/useFadingAnimation.ts
@@ -1,0 +1,50 @@
+import { Platform, Animated } from 'react-native';
+import { useState, useEffect } from 'react';
+
+function useFadingAnimation(
+  targetVisibility: boolean,
+  { toValue, delay, duration, easing }: Partial<Animated.TimingAnimationConfig>,
+) {
+  let [mounted, setMounted] = useState(false);
+  let [animatedValue] = useState(new Animated.Value(0));
+
+  useEffect(() => {
+    if (targetVisibility === mounted) {
+      return;
+    }
+
+    let canceled = false;
+    let animation = Animated.timing(animatedValue, {
+      toValue: toValue ?? targetVisibility ? 1 : 0,
+      delay,
+      duration,
+      easing,
+      // Native driver is not available on web.
+      useNativeDriver: Platform.OS !== 'web',
+    });
+
+    animation.start(() => {
+      if (canceled) {
+        return;
+      }
+      setMounted((m) => !m);
+    });
+
+    return () => {
+      canceled = true;
+      animation.stop();
+    };
+  }, [
+    mounted,
+    targetVisibility,
+    animatedValue,
+    toValue,
+    delay,
+    duration,
+    easing,
+  ]);
+
+  return [targetVisibility || mounted, animatedValue] as const;
+}
+
+export default useFadingAnimation;

packages/exoflex/src/helpers/__tests__/useFadingAnimation.test.ts
@@ -1,0 +1,39 @@
+import { renderHook } from '@testing-library/react-hooks';
+
+import useFadingAnimation from '../useFadingAnimation';
+
+describe('useFadingAnimation', () => {
+  it('should animate visibility properly', () => {
+    let { result, rerender, unmount } = renderHook(
+      ({ visible }) => useFadingAnimation(visible, { duration: 500 }),
+      { initialProps: { visible: false } },
+    );
+
+    expect(result.current[0]).toBe(false);
+
+    rerender({ visible: true });
+
+    expect(result.current[0]).toBe(true);
+
+    unmount();
+
+    // Should not modify the state again after unmounting.
+    expect(result.current[0]).toBe(true);
+  });
+  it(`should not animate when visibility doesn't change`, () => {
+    let { result, rerender, unmount } = renderHook(
+      ({ visible }) => useFadingAnimation(visible, { duration: 500 }),
+      { initialProps: { visible: false } },
+    );
+
+    expect(result.current[0]).toBe(false);
+
+    rerender({ visible: false });
+
+    expect(result.current[0]).toBe(false);
+
+    unmount();
+
+    expect(result.current[0]).toBe(false);
+  });
+});