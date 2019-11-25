b232f65210f028b3fabad5f35e2d2e8d6eb74546
Yoan Pratama Putra <yoanpratamaputra@gmail.com>
2019-11-25 15:11:06 +0700

    test(exoflex): create visual regression testing for remaining components

--- packages/exoflex/example/cypress/integration/visual.spec.ts
+++ packages/exoflex/example/cypress/integration/visual.spec.ts
@@ -13,6 +13,21 @@ describe('Visual Regression', () => {
     cy.percySnapshot();
   });
 
+  it('Accordion (Closed)', () => {
+    cy.visit(`${SITE}/accordion`);
+    cy.findAllByRole('img').should('have.length.greaterThan', 2);
+    cy.percySnapshot();
+  });
+
+  it('Accordion (Open)', () => {
+    cy.visit(`${SITE}/accordion`);
+    cy.findAllByRole('img').should('have.length.greaterThan', 0);
+    cy.queryByText('SECOND SECTION').click();
+    cy.findAllByRole('img').should('have.length.greaterThan', 1);
+    cy.wait(300);
+    cy.percySnapshot();
+  });
+
   it('Button', () => {
     cy.visit(`${SITE}/button`);
     cy.contains('WITH ICON');
@@ -26,9 +41,100 @@ describe('Visual Regression', () => {
     cy.percySnapshot();
   });
 
+  it('Calendar', () => {
+    cy.visit(`${SITE}/calendar`);
+    cy.findAllByRole('img').should('have.length.greaterThan', 0);
+    cy.wait(200);
+    cy.percySnapshot();
+  });
+
+  it('Checkbox (Unchecked)', () => {
+    cy.visit(`${SITE}/checkbox`);
+    cy.findAllByRole('img').should('have.length.greaterThan', 0);
+    cy.percySnapshot();
+  });
+
+  it('Checkbox (Checked)', () => {
+    cy.visit(`${SITE}/checkbox`);
+    cy.findAllByRole('img').should('have.length.greaterThan', 0);
+    cy.queryByText('Agree').click();
+    cy.percySnapshot();
+  });
+
+  it('Collapsible (Closed)', () => {
+    cy.visit(`${SITE}/collapsible`);
+    cy.findAllByRole('img').should('have.length.greaterThan', 1);
+    cy.percySnapshot();
+  });
+
+  it('Collapsible (Open)', () => {
+    cy.visit(`${SITE}/collapsible`);
+    cy.findAllByRole('img').should('have.length.greaterThan', 1);
+    cy.queryByText('Press Me').click();
+    cy.wait(300);
+    cy.percySnapshot();
+  });
+
+  it('DateTimePicker (Date)', () => {
+    cy.visit(`${SITE}/datetimepicker`);
+    cy.findAllByRole('img').should('have.length.greaterThan', 0);
+    cy.queryByText('OPEN DATEPICKER').click();
+    cy.percySnapshot();
+  });
+
+  it('DateTimePicker (Time)', () => {
+    cy.visit(`${SITE}/datetimepicker`);
+    cy.findAllByRole('img').should('have.length.greaterThan', 0);
+    cy.queryByText('OPEN TIMEPICKER').click();
+    cy.percySnapshot();
+  });
+
   it('IconButon', () => {
     cy.visit(`${SITE}/iconbutton`);
     cy.findAllByRole('img').should('have.length.greaterThan', 2);
+    cy.percySnapshot();
+  });
+
+  it('Modal', () => {
+    cy.visit(`${SITE}/modal`);
+    cy.findAllByRole('img').should('have.length.greaterThan', 0);
+    cy.queryByText('OPEN MODAL').click();
+    cy.findByText('Howdy, Modal!');
+    cy.wait(300);
+    cy.percySnapshot();
+  });
+
+  it('Portal', () => {
+    cy.visit(`${SITE}/portal`);
+    cy.findAllByRole('img').should('have.length.greaterThan', 0);
+    cy.queryByText('Press me to show the portal.').click();
+    cy.wait(300);
+    cy.percySnapshot();
+  });
+
+  xit('ProgressBar', () => {
+    cy.visit(`${SITE}/progressbar`);
+    cy.findAllByRole('img').should('have.length.greaterThan', 0);
+    cy.wait(500);
+    cy.percySnapshot();
+  });
+
+  it('RadioButton', () => {
+    cy.visit(`${SITE}/radiobuttongroup`);
+    cy.findAllByRole('img').should('have.length.greaterThan', 0);
+    cy.queryByText('Prefer not to say').click();
+    cy.percySnapshot();
+  });
+
+  it('Slider', () => {
+    cy.visit(`${SITE}/slider`);
+    cy.findAllByRole('img').should('have.length.greaterThan', 0);
+    cy.percySnapshot();
+  });
+
+  it('Switch', () => {
+    cy.visit(`${SITE}/switch`);
+    cy.findAllByRole('img').should('have.length.greaterThan', 0);
     cy.percySnapshot();
   });
 
@@ -43,6 +149,16 @@ describe('Visual Regression', () => {
     cy.findAllByRole('img').should('have.length.greaterThan', 1);
     cy.queryByPlaceholderText('Hint Text').type('This is using label');
     cy.queryByPlaceholderText('Type here...').type('This is not using label');
+    cy.percySnapshot();
+  });
+
+  it('Toast', () => {
+    cy.visit(`${SITE}/toast`);
+    cy.findAllByRole('img').should('have.length.greaterThan', 0);
+    cy.queryByText('TOGGLE CONTROLLED TOAST').click();
+    cy.findAllByRole('img').should('have.length.greaterThan', 1);
+    cy.findByText('Controlled Toast');
+    cy.wait(500);
     cy.percySnapshot();
   });
 
