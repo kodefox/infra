3e82bb1dd59ea4b67919e9fcec8ae8d0108d1a6a
Yoan Pratama Putra <yoanpratamaputra@gmail.com>
2019-12-11 15:02:29 +0700

    fix(exoflex): handle locale on native TimePicker (#216)

--- packages/exoflex/src/components/DateTimePicker/DateTimePicker.tsx
+++ packages/exoflex/src/components/DateTimePicker/DateTimePicker.tsx
@@ -9,6 +9,8 @@ export default function DateTimePicker(props: DateTimePickerProps) {
     onConfirm,
     date = new Date().toISOString(),
     mode = 'datetime' as DateTimePickerMode,
+    is24Hour = false,
+    locale,
     ...otherProps
   } = props;
 
@@ -16,6 +18,9 @@ export default function DateTimePicker(props: DateTimePickerProps) {
     <RNDateTimePicker
       mode={mode}
       date={date.trim() === '' ? new Date() : new Date(date)}
+      is24Hour={is24Hour}
+      // NOTE: If locale is not provided, use `en-GB` for 12h format
+      locale={locale || is24Hour ? 'en-GB' : 'en-US'}
       onCancel={() => onCancel()}
       onConfirm={(newDate) => onConfirm(newDate.toISOString())}
       {...otherProps}
