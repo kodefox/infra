a9c54aa2f36279edbdc62e1182f355f2e663dba0
Joshua <joshuasetiawan04@gmail.com>
2019-12-16 14:26:01 +0700

    feat: add useQuery

---
+++ packages/feetch/src/hooks/useQuery.ts
@@ -0,0 +1,41 @@
+import { useQuery as useLibraryQuery, Action } from 'react-fetching-library';
+
+import { Type } from 'io-ts';
+import { isLeft } from 'fp-ts/lib/Either';
+
+type QueryOption<T> = {
+  initFetch?: boolean;
+  schema: Type<T>;
+};
+
+type UseQueryResponse<T> = Omit<
+  ReturnType<typeof useLibraryQuery>,
+  'payload'
+> & { payload?: T };
+
+function useQuery<T>(
+  action: Action,
+  option: QueryOption<T>,
+): UseQueryResponse<T> {
+  let { payload, ...queryResult } = useLibraryQuery(action, option.initFetch);
+  //loading
+  if (queryResult.loading || queryResult.error) {
+    return { ...queryResult };
+  }
+  let decodedPayload = option.schema.decode(payload);
+  //if decode result in error
+  if (isLeft(decodedPayload)) {
+    return {
+      ...queryResult,
+      error: true,
+      errorObject: decodedPayload.left,
+    };
+  }
+  //successfull decode
+  return {
+    ...queryResult,
+    payload: payload,
+  };
+}
+
+export { useQuery };
---
+++ packages/feetch/src/index.ts
@@ -0,0 +1,4 @@
+import { ClientContextProvider, createClient } from 'react-fetching-library';
+import { useQuery } from './hooks/useQuery';
+
+export { ClientContextProvider, createClient, useQuery };
