db41a7a9d672551137ad639af0c48b8f43af4309
Yoan Pratama Putra <yoanpratamaputra@gmail.com>
2019-12-16 19:07:18 +0700

    test(exoflex): make test to cover both ripple and opacity button

--- packages/exoflex/src/components/__tests__/Button.test.native.tsx
+++ packages/exoflex/src/components/__tests__/Button.test.native.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Provider from '../Provider';
 import { render, fireEvent } from 'react-native-testing-library';
 import Button from '../Button';
+import Text from '../Text';
 
 describe('Button', () => {
   it('should render normally', () => {
@@ -9,10 +10,18 @@ describe('Button', () => {
       <Provider>
         <Button preset="primary">This is primary button</Button>
         <Button preset="secondary">This is secondary button</Button>
+        <Button useRipple preset="primary">
+          Primary ripple
+        </Button>
+        <Button useRipple preset="secondary">
+          Secondary ripple
+        </Button>
       </Provider>,
     );
     expect(getByText('THIS IS PRIMARY BUTTON')).toBeTruthy();
     expect(getByText('THIS IS SECONDARY BUTTON')).toBeTruthy();
+    expect(getByText('PRIMARY RIPPLE')).toBeTruthy();
+    expect(getByText('SECONDARY RIPPLE')).toBeTruthy();
   });
 
   it('should render normally with icon', () => {
@@ -21,16 +30,20 @@ describe('Button', () => {
         <Button preset="primary" icon="home">
           This is primary button with icon
         </Button>
+        <Button useRipple preset="primary" icon="home">
+          Primary ripple with icon
+        </Button>
       </Provider>,
     );
     expect(getByText('THIS IS PRIMARY BUTTON WITH ICON')).toBeTruthy();
+    expect(getByText('PRIMARY RIPPLE WITH ICON')).toBeTruthy();
   });
 
   it('should run onPress', () => {
     let mockPress = jest.fn();
     let { getByText } = render(
       <Provider>
-        <Button preset="primary" onPress={mockPress}>
+        <Button preset="invisible" color="red" onPress={mockPress}>
           Press me!
         </Button>
       </Provider>,
@@ -46,12 +59,18 @@ describe('Button', () => {
         <Button disabled preset="primary" onPress={mockPress}>
           Press me!
         </Button>
+        <Button disabled useRipple preset="primary" onPress={mockPress}>
+          <Text>Ripple me!</Text>
+        </Button>
       </Provider>,
     );
     let element = getByText('PRESS ME!');
+    let elementRipple = getByText('Ripple me!');
     // NOTE: Disable this as this is still an issue (https://github.com/callstack/react-native-testing-library/issues/28)
     // fireEvent.press(element);
+    // fireEvent.press(elementRipple);
     expect(element).toBeTruthy();
+    expect(elementRipple).toBeTruthy();
     expect(mockPress).toBeCalledTimes(0);
   });
 });
--- packages/exoflex/src/components/__tests__/Button.test.web.tsx
+++ packages/exoflex/src/components/__tests__/Button.test.web.tsx
@@ -3,6 +3,7 @@ import Provider from '../Provider';
 import { render, fireEvent } from '@testing-library/react';
 import '../../../test/customFireEvent.web';
 import Button from '../Button';
+import Text from '../Text';
 
 describe('Button', () => {
   it('should render normally', () => {
@@ -10,10 +11,18 @@ describe('Button', () => {
       <Provider>
         <Button preset="primary">This is primary button</Button>
         <Button preset="secondary">This is secondary button</Button>
+        <Button useRipple preset="primary">
+          Primary ripple
+        </Button>
+        <Button useRipple preset="secondary">
+          Secondary ripple
+        </Button>
       </Provider>,
     );
     expect(getByText('THIS IS PRIMARY BUTTON')).toBeTruthy();
     expect(getByText('THIS IS SECONDARY BUTTON')).toBeTruthy();
+    expect(getByText('PRIMARY RIPPLE')).toBeTruthy();
+    expect(getByText('SECONDARY RIPPLE')).toBeTruthy();
   });
 
   it('should render normally with icon', () => {
@@ -22,16 +31,20 @@ describe('Button', () => {
         <Button preset="primary" icon="home">
           This is primary button with icon
         </Button>
+        <Button useRipple preset="primary" icon="home">
+          Primary ripple with icon
+        </Button>
       </Provider>,
     );
     expect(getByText('THIS IS PRIMARY BUTTON WITH ICON')).toBeTruthy();
+    expect(getByText('PRIMARY RIPPLE WITH ICON')).toBeTruthy();
   });
 
   it('should run onPress', () => {
     let mockPress = jest.fn();
     let { getByText } = render(
       <Provider>
-        <Button preset="primary" onPress={mockPress}>
+        <Button preset="invisible" color="red" onPress={mockPress}>
           Press me!
         </Button>
       </Provider>,
@@ -47,11 +60,17 @@ describe('Button', () => {
         <Button disabled preset="primary" onPress={mockPress}>
           Press me!
         </Button>
+        <Button disabled useRipple preset="primary" onPress={mockPress}>
+          <Text>Ripple me!</Text>
+        </Button>
       </Provider>,
     );
     let element = getByText('PRESS ME!');
+    let elementRipple = getByText('Ripple me!');
     fireEvent.click(element);
+    fireEvent.click(elementRipple);
     expect(element).toBeTruthy();
+    expect(elementRipple).toBeTruthy();
     expect(mockPress).toBeCalledTimes(0);
   });
 });
