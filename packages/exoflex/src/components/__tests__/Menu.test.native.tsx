6c995151c03d1e30212eaa8a1f3555d0f6874e86
Yoan Pratama Putra <yoanpratamaputra@gmail.com>
2020-01-13 16:20:03 +0700

    test(exoflex): menu component

---
+++ packages/exoflex/src/components/__tests__/Menu.test.native.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import Provider from '../Provider';
+import {
+  render,
+  fireEvent,
+  act,
+  waitForElement,
+} from 'react-native-testing-library';
+import Menu from '../Menu';
+import Text from '../Text';
+import Button from '../Button';
+
+describe('Menu.Item', () => {
+  it('should render properly', () => {
+    let { getByText } = render(
+      <Provider>
+        <Menu.Item title="Item 1" />
+      </Provider>,
+    );
+    expect(getByText('Item 1')).toBeTruthy();
+  });
+
+  it('should render properly with custom title', () => {
+    let { getByText } = render(
+      <Provider>
+        <Menu.Item title={<Text>Custom Item</Text>} />
+      </Provider>,
+    );
+    expect(getByText('Custom Item')).toBeTruthy();
+  });
+});
+
+describe('Menu', () => {
+  it('should render properly', async () => {
+    const App = () => {
+      let [visible, setVisible] = useState(false);
+      return (
+        <Provider>
+          <Menu
+            visible={visible}
+            onDismiss={jest.fn()}
+            anchor={<Button onPress={() => setVisible(true)}>Show menu</Button>}
+          >
+            <Menu.Item title="Item 1" />
+          </Menu>
+        </Provider>
+      );
+    };
+
+    let { getByText } = render(<App />);
+
+    act(() => {
+      fireEvent.press(getByText('Show menu'));
+    });
+    await waitForElement(() => getByText('Item 1'));
+
+    expect(getByText('Item 1')).toBeTruthy();
+  });
+});
---
+++ packages/exoflex/src/components/__tests__/Menu.test.web.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import Provider from '../Provider';
+import { render, fireEvent, act, wait } from '@testing-library/react';
+import '../../../test/customFireEvent.web';
+import Menu from '../Menu';
+import Text from '../Text';
+import Button from '../Button';
+
+describe('Menu.Item', () => {
+  it('should render properly', () => {
+    let { getByText } = render(
+      <Provider>
+        <Menu.Item title="Item 1" />
+      </Provider>,
+    );
+    expect(getByText('Item 1')).toBeTruthy();
+  });
+
+  it('should render properly with custom title', () => {
+    let { getByText } = render(
+      <Provider>
+        <Menu.Item title={<Text>Custom Item</Text>} />
+      </Provider>,
+    );
+    expect(getByText('Custom Item')).toBeTruthy();
+  });
+});
+
+describe('Menu', () => {
+  it('should render properly', async () => {
+    const App = () => {
+      let [visible, setVisible] = useState(false);
+      return (
+        <Provider>
+          <Menu
+            visible={visible}
+            onDismiss={jest.fn()}
+            anchor={<Button onPress={() => setVisible(true)}>Show menu</Button>}
+          >
+            <Menu.Item title="Item 1" />
+          </Menu>
+        </Provider>
+      );
+    };
+
+    let { getByText } = render(<App />);
+
+    act(() => {
+      fireEvent.click(getByText('Show menu'));
+    });
+    await wait(() => getByText('Item 1'));
+
+    expect(getByText('Item 1')).toBeTruthy();
+  });
+});
