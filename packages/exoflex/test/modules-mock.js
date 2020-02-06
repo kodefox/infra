f556ea862941f0bf30e766af21cfa0ceba26dc07
Yosua Ian Sebastian <yosua.ian.sebastian@gmail.com>
2020-02-07 00:35:06 +0700

    fix(exoflex): fix wrong file extension in module entrypoint (#334)
    * fix: use correct file extension for module entrypoint
    
    * chore: remove unused noEmit in tsconfig
    
    * chore: fix mock NativeAnimatedHelper breaking jest tests

--- packages/exoflex/test/modules-mock.js
+++ packages/exoflex/test/modules-mock.js
@@ -1,8 +1,7 @@
 jest.mock('react-native-svg', () => 'Svg');
-jest.mock('NativeAnimatedHelper');
 
 // Hide warning about componentWillReceiveProps has been renamed
-jest.spyOn(console, 'warn').mockImplementation((message: string) => {
+jest.spyOn(console, 'warn').mockImplementation((message) => {
   let blacklistedMessage =
     'Warning: componentWillReceiveProps has been renamed';
   return message.startsWith(blacklistedMessage) ? '' : message;
