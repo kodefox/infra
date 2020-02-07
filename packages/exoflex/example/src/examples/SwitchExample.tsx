e3847d747efbb4911f5b5b43f888a6eb861a471c
Yosua Ian Sebastian <yosua.ian.sebastian@gmail.com>
2020-02-07 14:37:12 +0700

    chore: remove unused import from example

--- packages/exoflex/example/src/examples/SwitchExample.tsx
+++ packages/exoflex/example/src/examples/SwitchExample.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { ScrollView, StyleSheet, View } from 'react-native';
-import { Switch, Text, Button } from 'exoflex';
+import { Switch, Text } from 'exoflex';
 
 function SwitchExample() {
   let [switchValue, setSwitchValue] = useState(false);
