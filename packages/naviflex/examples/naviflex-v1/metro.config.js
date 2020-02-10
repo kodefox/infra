049a8d9d1fddbaf656424f0a1ad7d0a917cdcf6e
Yoan Pratama Putra <yoanpratamaputra@gmail.com>
2020-02-10 13:55:02 +0700

    chore(naviflex): add new example for react-navigation v5

---
+++ packages/naviflex/examples/naviflex-v1/metro.config.js
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const path = require("path");
+const blacklist = require("metro-config/src/defaults/blacklist");
+const pak = require("../../package.json");
+const escape = require("escape-string-regexp");
+
+const dependencies = Object.keys(pak.dependencies);
+
+let extraNodeModules = {};
+["react-native", "react", "@react-navigation/native", ...dependencies].forEach(
+  dep => {
+    extraNodeModules[dep] = path.resolve(__dirname, "node_modules", dep);
+  }
+);
+
+module.exports = {
+  projectRoot: __dirname,
+  watchFolders: [path.resolve(__dirname, "../..")],
+
+  resolver: {
+    blacklistRE: blacklist([
+      new RegExp(
+        `^${escape(path.resolve(__dirname, "../..", "node_modules"))}\\/.*$`
+      )
+    ]),
+
+    extraNodeModules
+  }
+};
