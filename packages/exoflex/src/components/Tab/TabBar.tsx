2bc3939c8ad6869131f7bcf35658b17ea44dd15f
Yoan Pratama Putra <yoanpratamaputra@gmail.com>
2020-02-18 14:21:03 +0700

    feat(exoflex): custom tab indicator styling

--- packages/exoflex/src/components/Tab/TabBar.tsx
+++ packages/exoflex/src/components/Tab/TabBar.tsx
@@ -15,10 +15,18 @@ type TabBarProps = {
   onTabPress: (index: number) => void;
   style?: StyleProp<ViewStyle>;
   textStyle?: StyleProp<TextStyle>;
+  indicatorStyle?: StyleProp<ViewStyle>;
 };
 
 export default function TabBar(props: TabBarProps) {
-  let { activeIndex, titles, onTabPress, style, textStyle } = props;
+  let {
+    activeIndex,
+    titles,
+    onTabPress,
+    style,
+    textStyle,
+    indicatorStyle,
+  } = props;
 
   let [width, setWidth] = useState(0);
 
@@ -33,6 +41,7 @@ export default function TabBar(props: TabBarProps) {
         width={indicatorWidth}
         activeIndex={activeIndex}
         maxIndex={titles.length}
+        style={indicatorStyle}
       />
       {titles.map((title, index) => (
         <TabItem
