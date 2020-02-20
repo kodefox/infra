8c5ed9a4f08da1cc866498e41197220906f8fe4a
Joshua <joshuasetiawan04@gmail.com>
2020-02-20 12:25:02 +0700

    test: add fixture test for mutation

--- packages/feetch/src/__tests__/useMutation.test.tsx
+++ packages/feetch/src/__tests__/useMutation.test.tsx
@@ -2,14 +2,36 @@ import React, { ReactNode, ComponentType } from 'react';
 import { renderHook, act } from '@testing-library/react-hooks';
 import { Record, String } from 'runtypes';
 import fetchMock from 'fetch-mock';
+import 'isomorphic-fetch';
 
 import { createClient, useMutation, ClientContextProvider, Action } from '../';
 
-let client = createClient();
+let client = createClient({});
 let FetchProvider: ComponentType = ({ children }: { children?: ReactNode }) => (
   <ClientContextProvider client={client}>{children}</ClientContextProvider>
 );
 
+let clientWithFixtures = createClient({
+  fixtures: [
+    {
+      method: 'POST',
+      endpoint: '/messages',
+      responseBody: {
+        id: 'postID_jd91dh83h9d3esd',
+      },
+    },
+  ],
+});
+
+let FetchProviderWithFixture: ComponentType = ({
+  children,
+}: {
+  children?: ReactNode;
+}) => (
+  <ClientContextProvider client={clientWithFixtures}>
+    {children}
+  </ClientContextProvider>
+);
 describe('useMutation', () => {
   afterEach(() => {
     fetchMock.reset();
@@ -117,5 +139,37 @@ describe('useMutation', () => {
     expect(result.current.errorObject.message).toBe(
       'Expected string, but was number',
     );
+  });
+
+  it('should successfully mutate with fixture', async () => {
+    const postMessageAction = (body: string): Action => ({
+      method: 'POST',
+      endpoint: '/messages',
+      body,
+    });
+
+    const Return = Record({
+      id: String,
+    });
+
+    let body = JSON.stringify({ name: 'John', message: 'This is message' });
+    let mockPayload = { id: 'postID_jd91dh83h9d3esd' };
+
+    let { result, waitForNextUpdate } = renderHook(
+      () => useMutation(postMessageAction, { schema: Return }),
+      { wrapper: FetchProviderWithFixture },
+    );
+
+    expect(result.current.loading).toBeFalsy();
+
+    act(() => {
+      result.current.mutate(body);
+    });
+    expect(result.current.loading).toBeTruthy();
+
+    await waitForNextUpdate();
+    expect(result.current.loading).toBeFalsy();
+    expect(result.current.status).toBe(200);
+    expect(result.current.payload).toEqual(mockPayload);
   });
 });
