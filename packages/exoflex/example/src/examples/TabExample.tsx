14de5366624809c4d6a1852180b3c4f51a34a5c8
Yoan Pratama Putra <yoanpratamaputra@gmail.com>
2020-03-06 17:18:34 +0700

    chore(exoflex): tab view example

--- packages/exoflex/example/src/examples/TabExample.tsx
+++ packages/exoflex/example/src/examples/TabExample.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Dimensions, Image, ScrollView } from 'react-native';
-import { TabView as RNTabView, SceneMap } from 'react-native-tab-view';
+import { StyleSheet, View, Image, ScrollView } from 'react-native';
 import { TabView, Button } from 'exoflex';
 
 const imageUri =
@@ -10,6 +9,7 @@ let images = [];
 for (let i = 0; i < 100; i++) {
   images.push(
     <Image
+      key={i.toString()}
       source={{ uri: imageUri }}
       style={{ width: '100%', height: 200, marginVertical: 10 }}
     />,
@@ -19,8 +19,11 @@ for (let i = 0; i < 100; i++) {
 const FirstRoute = () => (
   <ScrollView
     contentContainerStyle={[
-      styles.scene,
-      { backgroundColor: '#ff4081', padding: 10, paddingHorizontal: 20 },
+      {
+        backgroundColor: '#ff4081',
+        padding: 10,
+        paddingHorizontal: 20,
+      },
     ]}
   >
     {images}
@@ -31,7 +34,7 @@ const SecondRoute = () => (
   <View style={[styles.scene, { backgroundColor: '#673ab7' }]} />
 );
 
-const ThirdRoute = (props) => {
+const ThirdRoute = (props: {jumpTo: (index: number) => void}) => {
   return (
     <View style={[styles.scene, { backgroundColor: 'skyblue' }]}>
       <Button onPress={() => props.jumpTo(0)} style={{ marginTop: 100 }}>
@@ -49,28 +52,9 @@ const FifthRoute = () => (
   <View style={[styles.scene, { backgroundColor: 'goldenrod' }]} />
 );
 
-// const initialLayout = { width: Dimensions.get('window').width };
 
 function TabExample() {
   const [index, setIndex] = useState(0);
-  // const [routes] = React.useState([
-  //   { key: 'first', title: 'First' },
-  //   { key: 'second', title: 'Second' },
-  //   { key: 'third', title: 'Third' },
-  //   { key: 'fourth', title: 'Fourth' },
-  // ]);
-
-  // const renderScene = SceneMap({
-  //   first: FirstRoute,
-  //   second: SecondRoute,
-  // });
-
-  // let scenes = {
-  //   first: FirstRoute,
-  //   second: SecondRoute,
-  //   third: ThirdRoute,
-  //   fourth: FourthRoute,
-  // };
 
   let scenes = [
     { title: 'First', scene: FirstRoute },
@@ -87,33 +71,14 @@ function TabExample() {
       activeIndex={index}
       scenes={scenes}
       onIndexChange={setIndex}
+      // NOTE: Intentionally left these commented for easier development purpose
       // tabItemStyle={{ backgroundColor: 'white' }}
       // tabItemTextStyle={{ color: 'red' }}
       // tabIndicatorStyle={{ backgroundColor: 'brown' }}
       // style={{ backgroundColor: 'white' }}
     />
   );
-
-  // return (
-  //   <RNTabView
-  //     navigationState={{ index, routes }}
-  //     renderScene={renderScene}
-  //     onIndexChange={setIndex}
-  //     initialLayout={initialLayout}
-  //   />
-  // );
 }
-
-// function TabHooksExample() {
-//   const [routes] = React.useState([
-//     { key: 'first', title: 'First', scene: FirstRoute },
-//     { key: 'second', title: 'Second', scene: SecondRoute },
-//   ]);
-
-//   let {TabView, jumpTo, routes} = useTabView(routes);
-
-//   return <TabView />;
-// }
 
 TabExample.title = 'Tab';
 
