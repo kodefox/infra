610f9a94dd22d1633bacf893798b7c1cfec59b8c
Yoan Pratama Putra <yoanpratamaputra@gmail.com>
2020-04-23 13:43:36 +0700

    chore: fix pre-push script to functions better (#443)

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -1,11 +1,14 @@
 ROOT_DIR=$(pwd | sed 's/infra.*/infra/')
+echo ">> Running pre-push hook for testing related package to the commits"
 cd $ROOT_DIR
 for package in packages/*; do
   if [[ $(git diff master..HEAD --name-only | grep $package) != "" && $package != "packages/eslint-config-kodefox" ]]; then
+    echo ">> Will start testing $package"
     cd $package
     yarn test
     if [[ $? != 0 ]]; then
       exit 1
     fi
+    cd $ROOT_DIR
   fi
 done
