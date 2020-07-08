fbf5947e76dfebc0b618bd56dad07a2e73b83ca0
Yoan Pratama Putra <yoanpratamaputra@gmail.com>
2020-07-08 14:29:25 +0700

    test(exoflex): change tests to handle the refactor

--- packages/exoflex/src/components/__tests__/Switch.test.tsx
+++ packages/exoflex/src/components/__tests__/Switch.test.tsx
@@ -4,17 +4,17 @@ const DEFAULT_WIDTH = 48;
 const WIDTH = 100;
 
 describe('getTrueWidth', () => {
-  it('should return default width', () => {
-    let width = getTrueWidth('track', DEFAULT_WIDTH);
-    let thumbSize = getTrueWidth('thumb', width);
+  it('should return base width', () => {
+    let width = getTrueWidth(DEFAULT_WIDTH);
+    let thumbSize = getTrueWidth(width / 2);
 
     expect(width).toBe(DEFAULT_WIDTH);
     expect(thumbSize).toBe(width / 2);
   });
 
   it('should return width', () => {
-    let width = getTrueWidth('track', DEFAULT_WIDTH, { width: WIDTH });
-    let thumbSize = getTrueWidth('thumb', width, { width: WIDTH });
+    let width = getTrueWidth(DEFAULT_WIDTH, { width: WIDTH });
+    let thumbSize = getTrueWidth(width / 2, { width: WIDTH });
 
     expect(width).toBe(WIDTH);
     expect(thumbSize).toBe(WIDTH);
@@ -24,10 +24,10 @@ describe('getTrueWidth', () => {
     const MIN_WIDTH_A = 60;
     const MIN_WIDTH_B = 120;
 
-    let widthA = getTrueWidth('track', DEFAULT_WIDTH, {
+    let widthA = getTrueWidth(DEFAULT_WIDTH, {
       minWidth: MIN_WIDTH_A,
     });
-    let widthB = getTrueWidth('track', DEFAULT_WIDTH, {
+    let widthB = getTrueWidth(DEFAULT_WIDTH, {
       width: WIDTH,
       minWidth: MIN_WIDTH_B,
     });
@@ -40,10 +40,10 @@ describe('getTrueWidth', () => {
     const MAX_WIDTH_A = 30;
     const MAX_WIDTH_B = 80;
 
-    let widthA = getTrueWidth('track', DEFAULT_WIDTH, {
+    let widthA = getTrueWidth(DEFAULT_WIDTH, {
       maxWidth: MAX_WIDTH_A,
     });
-    let widthB = getTrueWidth('track', DEFAULT_WIDTH, {
+    let widthB = getTrueWidth(DEFAULT_WIDTH, {
       width: WIDTH,
       maxWidth: MAX_WIDTH_B,
     });
@@ -52,23 +52,25 @@ describe('getTrueWidth', () => {
     expect(widthB).toBe(MAX_WIDTH_B);
   });
 
-  it('should return maxWidth even when minWidth is bigger', () => {
+  // NOTE: Follows CSS Standards
+  // https://www.w3.org/TR/CSS21/visudet.html#min-max-widths
+  it('should return minWidth even when maxWidth is bigger', () => {
     const MIN_WIDTH_A = 60;
     const MIN_WIDTH_B = 120;
     const MAX_WIDTH_A = 30;
     const MAX_WIDTH_B = 80;
 
-    let widthA = getTrueWidth('track', DEFAULT_WIDTH, {
+    let widthA = getTrueWidth(DEFAULT_WIDTH, {
       minWidth: MIN_WIDTH_A,
       maxWidth: MAX_WIDTH_A,
     });
-    let widthB = getTrueWidth('track', DEFAULT_WIDTH, {
+    let widthB = getTrueWidth(DEFAULT_WIDTH, {
       width: WIDTH,
       minWidth: MIN_WIDTH_B,
       maxWidth: MAX_WIDTH_B,
     });
 
-    expect(widthA).toBe(MAX_WIDTH_A);
-    expect(widthB).toBe(MAX_WIDTH_B);
+    expect(widthA).toBe(MIN_WIDTH_A);
+    expect(widthB).toBe(MIN_WIDTH_B);
   });
 });
