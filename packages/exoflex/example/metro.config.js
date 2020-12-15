ba0463a193bf0148194894d68650bd4f0bbda372
Yosua Ian Sebastian <yosua.ian.sebastian@gmail.com>
2020-12-15 19:06:22 +0700

    refactor(exoflex): import modular lodash package instead of whole lodash package (#584)

--- packages/exoflex/example/metro.config.js
+++ packages/exoflex/example/metro.config.js
@@ -14,9 +14,6 @@ let extraNodeModules = {};
   'react-native-svg',
   'expo-font',
   '@babel/runtime',
-  // We probably shouldn't use the entire lodash package here.
-  // TODO: Remove direct import from lodash in exoflex.
-  'lodash',
   ...dependencies,
 ].forEach((dep) => {
   extraNodeModules[dep] = path.resolve(__dirname, 'node_modules', dep);
